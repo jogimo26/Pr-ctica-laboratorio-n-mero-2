8ddd37d20b666cb5e81219131bb4e43575326601
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-10-16 14:53:24 -0500

    Add files via upload

---
+++ reto5/Ejercicio5.py
@@ -0,0 +1,4 @@
+# Resistencias por medio de un bucle
+# Se usa un bucle while para manejar la parte de la elección por parte del usuario
+print("Ingrese un número con el cual calcular las resistencias, las elecciones son: \n 1. Resistencias en serie \n 2. Resistencias en paralelo \n 3. Resistencias mixtas (R1, R2 en serie, R3 en paralelo) \n 4. Resistencias mixtas (R1, R3 en serie, R2 en paralelo) \n 5. Resistencias mixtas (R2, R3 en serie, R1 en paralelo)")
+variable = int(input("Ingrese el "))
