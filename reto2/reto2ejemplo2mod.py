33671d67d301181c3d1f203b5b8359a80ff3bb43
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-10-18 18:58:33 -0500

    Update reto2ejemplo2mod.py

--- reto2/reto2ejemplo2mod.py
+++ reto2/reto2ejemplo2mod.py
@@ -3,7 +3,7 @@ limite = int(input("Ingrese el límite de los números impares siguientes a ver:
 contador = 1
 print("Los números impares siguientes son: \n")
 
-for i in range(numero,limite,1): # Bucle de inicio a fin
+for i in range(numero,limite,1): # Bucle de inicio a límite
     if i % 2 == 0: # Verificar si es par, si no lo es se omite esto
         print(i,end="\n")
     contador += 1
