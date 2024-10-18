0f964919c35c7f84e54ded79fa3eaf468884dd78
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-10-18 18:53:44 -0500

    Update ejemplo2mod.py

--- Reto 1/ejemplo2mod.py
+++ Reto 1/ejemplo2mod.py
@@ -1,6 +1,6 @@
 contador = 1
 inicio = int(input("Ingrese el número desde donde desea ver los ID de inventario: "))
-for contador in range(inicio,inicio+10,1):
+for contador in range(inicio,inicio+10,1): # Modificación al ejemplo 2, lo que hace es, dentro de un rango comprendido entre inicio y inicio+10 sacar la ID de inventario
     print(f"El ID de inventario es: {contador}")
     contador += 1
 print("Hemos culminado!... Hasta pronto!")
