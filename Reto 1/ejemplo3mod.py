fc32ec89d409f716fec874686272185b47763832
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-10-18 18:54:39 -0500

    Update ejemplo3mod.py

--- Reto 1/ejemplo3mod.py
+++ Reto 1/ejemplo3mod.py
@@ -2,10 +2,10 @@ inicio = int(input("Proporcione el inicio del rango para el cual se van a ver lo
 fin = int(input("Proporcione el final del rango para el cual se van a ver los números pares: "))
 print("Los números pares en el rango indicado son: \n")
 
-if inicio % 2 == 0:
+if inicio % 2 == 0: # Si el número es par entonces va de inicio a fin con incremento 2
     for i in range(inicio,fin,2):
         print(i,end=" ")
 else:
-    for i in range(inicio+1,fin,2):
+    for i in range(inicio+1,fin,2): #Si el número es impar le suma 1 a inicio (para que quede par) y va hasta al fin, con step de 2
         print(i,end=" ")
 print("\nHasta aquí!!!")
