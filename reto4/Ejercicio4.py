0dc59486e7a2c4bd112c1ae8a0ee42f4789778aa
jogimo26 <joseph.gilm@uqvirtual.edu.co>
2024-10-18 19:22:41 -0500

    Update Ejercicio4.py

--- reto4/Ejercicio4.py
+++ reto4/Ejercicio4.py
@@ -1,14 +1,13 @@
-# Sistema de control de temperatura para un horno industrial manteniendo la temperatura dentro de un rango de más o menos 10% de la temperatura deseada
 import random
 
 TemperaturaDeseada = int(input("Ingrese la temperatura deseada para el horno: "))
-Tmax = TemperaturaDeseada + (TemperaturaDeseada*(10/100))
-Tmin = TemperaturaDeseada - (TemperaturaDeseada*(10/100))
-TemperaturaActual = random.randint(1,300)
+Tmax = TemperaturaDeseada + (TemperaturaDeseada*(10/100)) # Temperatura de error máxima
+Tmin = TemperaturaDeseada - (TemperaturaDeseada*(10/100)) # Temperatura de error mínima
+TemperaturaActual = random.randint(1,300) # Da una temperatura actual al azar al horno
 print(f"La temperatura actual del horno es {TemperaturaActual}")
 
-for i in range(1,TemperaturaDeseada,1): #usar un bucle while
+for i in range(1,TemperaturaDeseada,1): # Usar un bucle while
     Potencia = 0.1*(TemperaturaDeseada-TemperaturaActual)
-    print(f"La potencia de calentamiento es {Potencia}")
-    TemperaturaActual += Potencia
+    print(f"La potencia de calentamiento es: {Potencia}")
+    TemperaturaActual += Potencia # Se le añade la potencia, porque si se multiplica llegaría eventualmente la temperatura a infinito y la potencia a -infinito
     print(f"La temperatura actual es: {TemperaturaActual}")
